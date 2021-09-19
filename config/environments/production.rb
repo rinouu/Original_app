c3781ae02efe9521ebe6964cf3bd3c25f8ac8869
Corine <corine@localhost.localdomain>
2021-09-19 20:12:13 +0100

    config letter opener heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "original_app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'obscure-fortress-06608.herokuapp.com/'}
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
