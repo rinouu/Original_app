d706bd5c4e95675ee10d5f7772e239a10388a8b8
Corine <corine@localhost.localdomain>
2021-09-21 19:08:27 +0100

    Seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-categories = ["Career follow-up", "relational","punctuality"]
+categories = ["Career follow-up", "relational","punctuality","Resource management", "supplier faults"]
 categories.each do |category|
 	Category.create!( 
 	    name: category,
